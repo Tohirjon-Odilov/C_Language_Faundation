f146daec7db99a7982a02c4d251a498836a3a52f
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-07-08 16:07:32 +0500

    update some files

--- a_darsdan_tashqari/piramida.c
+++ a_darsdan_tashqari/piramida.c
@@ -2,7 +2,38 @@
 
 int main()
 {
+
 #if 1
+
+	int num;
+	int isTrue;
+
+	printf("Keling piramida yasaymiz:\nRozi bo'lsez 1 ni kiriting: ");
+	scanf("%d", &isTrue);
+
+	if (isTrue == 1)
+	{
+		printf("5 dan katta 10 kichik son kiriting: ");
+		scanf("%d", &num);
+		for (int i = 0; i < num; i++)
+		{
+			for (int j = 0; j <= i; j++)
+			{
+				printf("%d ", i + 1);
+			}
+			printf("\n");
+		}
+	}
+	else
+	{
+		printf("Boshqa harakat qiling!");
+	}
+
+	return 0;
+
+#endif
+
+#if 0
 
 	int n;
 	scanf("%d", &n);
@@ -20,28 +51,7 @@ int main()
 		r--;
 	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #endif
-
 
 #if 0
 	int n;
