6849b347f007bd69a6007dfc4af7647639ff1de5
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-07-11 17:56:42 +0500

    update code

--- c_015_arrat_f17_f23/f20.c
+++ c_015_arrat_f17_f23/f20.c
@@ -11,27 +11,24 @@ void output(int n, int arr[]){
 	puts("");
 }
 
-void push_arr(int n, int arr[], int k){
-	int j = 0;
-	for(int i = 0; i < k; i++){
-		j = arr[n-1];
-		for(int j = n - 1; j > 0; j--)
-			arr[j] = arr[j-1];
-		arr[0] = j;
-	}
+void push_arr(int n, int arr[]){
+	int j = arr[n-1];
+	for(int j = n - 1; j > 0; j--) arr[j] = arr[j-1];
+	arr[0] = j;
 }
 
 int main()
 {
 	srand(time(0));
-	int n, k;
-	scanf("%d", &n);
+	int n,k;
+	scanf("%d %d", &n, &k);
 	int arr[n];
-	scanf("%d", &k);
 
 	fill_arr(n, arr);
 	output(n, arr);
-	push_arr(n,arr,k);
+
+	k %= n;
+	for(int i = 0; i < k; i++) push_arr(n,arr);
 	output(n,arr);
 
 	return 0;
