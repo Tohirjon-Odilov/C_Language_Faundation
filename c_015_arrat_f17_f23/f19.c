ae0ed3a622ccf4ac28691b4a1cc9458e0d665ac3
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-07-11 15:59:21 +0500

    update to functions

--- c_015_arrat_f17_f23/f19.c
+++ c_015_arrat_f17_f23/f19.c
@@ -2,21 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 
-void fill_arr(int n, int arr[], int k, int new_arr[]){
-	for(int i = 0; i < n; i++){
+void fill_arr(int n, int arr[]){
+	for(int i = 0; i < n; i++)
 		arr[i] = 8 + rand() % 13;
-		if(i < k)
-			new_arr[i] = arr[i];
-	}
 }
 
-void change(int n, int arr[], int k, int new_arr[]){
-	int j = 0;
-	for(int i = 0; i < n; i++)
-		if(i < n-k) arr[i] = arr[i+k];
-
-	for(int i = n-k; i < n; i++)
-		arr[i] = new_arr[j++];
+void change(int n, int arr[], int k){
+	int temp;
+	for(int i = 0; i < k; i++){
+		temp = arr[0];
+		for(int j = 0; j < n-1; j++)
+			arr[j] = arr[j+1];
+		arr[n-1] = temp;
+	}
 }
 
 void output(int n, int arr[]){
@@ -28,14 +26,13 @@ void output(int n, int arr[]){
 int main()
 {
 	srand(time(0));
-	int n, k = 1;
+	int n, k;
 	scanf("%d %d", &n, &k);
 	int arr[n];
-	int new_arr[k];
 
-	fill_arr(n,arr,k,new_arr);
+	fill_arr(n,arr);
 	output(n,arr);
-	change(n,arr,k,new_arr);
+	change(n,arr,k);
 	output(n,arr);
 
 	return 0;
