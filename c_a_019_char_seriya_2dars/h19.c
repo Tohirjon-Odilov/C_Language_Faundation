f58280ede5f076c45c52d739a62f0159dd1f2b28
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-07-26 08:57:32 +0500

    add folder

---
+++ c_a_019_char_seriya_2dars/h19.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char name[32];
+	char second_name[32];
+	scanf("%s", name);
+	scanf("%s", second_name);
+
+	strcpy(second_name+strlen(second_name)/2, "");
+	strcpy(second_name+strlen(second_name), name+strlen(name)/2);
+
+	puts(second_name);
+
+	return 0;
+}
