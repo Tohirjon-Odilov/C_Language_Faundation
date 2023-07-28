4027c093e98d1fa2425c1c22a1124d6db2271a77
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-07-28 10:25:20 +0500

    o'zim qayta yozishim kerak

--- c_022_char_seriya_3dars_h19/h24.c
+++ c_022_char_seriya_3dars_h19/h24.c
@@ -1,38 +1,44 @@
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
+#include<stdio.h>
+#include<string.h>
 
-int string_to_number(char date){
-	int num = 0;
-	if(date != '.'){
-		num += num * 10 + (date - '0');
-		printf("%d", num);
-	}
-	else{
-		puts("");
-//		num = 0;
-	}
-	return num;
-}
+int main(){
 
-int main()
-{
-	char date[20];
-	scanf("%s", date);
+    char date[11];
+    printf("Enter date: ");
+    scanf("%s", date);
 
-	int num;
-	int check = 0;
+    int day = 0, month = 0, year = 0;
+    int value = 0;
 
-	for(int i = 0; date[i]; i++){
-		num = string_to_number(date[i]);
-		if(0 < num && num <= 31 && i == 2) check++;
-		if(0 < num <= 12 && i == 5) check++;
-		if(0 < num && 5 < i ) check++;
-		printf("%d", check);
-	}
+    for(int i = 0; date[i]; i++){
+        if(date[i] != 46){
+            value = value * 10 + (date[i] - 48);
+        }
+        else if(date[i] == 46){
+            if(i == 2){
+                day = value;
+            }else if(i == 5){
+                month = value;
+            }
+            value = 0;
+        }
+        if(i == 9){
+            year = value;
+        }
+    }
 
-	if(check == 3) puts("To'g'ri");
-	else puts("Noto'g'ri");
+    int check = 0;
+    if(1 <= day && day <= 31){
+        check++;
+    }if(1 <= month && month <= 12){
+        check++;
+    }if(0 < year && year <= 2023){
+        check++;
+    }
+    
+    if(check == 3){
+        printf("To'g'ri\n");
+    }else printf("Noto'g'ri\n");
 
-	return 0;
+    return 0;
 }
