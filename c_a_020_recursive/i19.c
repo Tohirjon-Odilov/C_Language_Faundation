22236433ca9c0750c8ac8bf0b34e3b5b91559f60
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-07-29 12:08:50 +0500

    fixed

--- c_a_020_recursive/i19.c
+++ c_a_020_recursive/i19.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int Ekub(int a, int b){
-	if(a>b)Ekub(a-b,b);
-	else if (a<b)Ekub(a,b-a);
-	else if(a == b)return a;
-	else if(a == 1 || b == 1) return 1;
+    if(b == 0) {
+            return a;
+    }
+    else {
+        return Ekub(b, a % b);
+    }
 }
 
 int main()
