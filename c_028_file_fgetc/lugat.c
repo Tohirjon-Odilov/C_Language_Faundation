a181b3c2e64abbce5d8bb6b04762b74dfc60b9e9
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-08-10 08:01:07 +0500

    update

--- c_028_file_fgetc/lugat.c
+++ c_028_file_fgetc/lugat.c
@@ -16,16 +16,20 @@ void display(){
 	printf(">>> ");
 }
 
+char *to_lower(char word[]){
+	for(int i = 0; word[i] != '\0'; i++)
+		word[i] = tolower(word[i]);
+	return word;
+}
+
 void add_new_word(){
 	WORD word;
 	FILE *f = fopen("dictionary.txt", "a");
 	printf("English: "); scanf("%s", word.eng);
-	for(int i = 0; word.eng[i] != '\0'; i++)
-		word.eng[i] = tolower(word.eng[i]);
+	strcpy(word.eng, to_lower(word.eng));
 
 	printf("Uzbek: "); scanf("%s", word.uzb);
-	for(int i = 0; word.uzb[i] != '\0'; i++)
-		word.uzb[i] = tolower(word.uzb[i]);
+	strcpy(word.uzb, to_lower(word.uzb));
 
 	fprintf(f, "%s %s\n", word.eng, word.uzb);
 	fclose(f);
@@ -46,12 +50,52 @@ void show_words(){
 
 	fclose(fr);
 }
-
-void search_word(){
+#if 0
+void translate(char sentence[], int lang){
+	FILE *fr = fopen("dictionary.txt", "r");
 	char eng[32], uzb[32];
+	WORD word;
+	int i = 0;
+//	if(!strcmp(lang, uzb)){
+//		
+//	}
+//	strcpy(word."lang", to_lower(sentence));
+
+	puts("+---------------+---------------+");
+	puts("| ENGLISH       | UZBEK         |");
+	puts("+---------------+---------------+");
+	if(lang == 2){
+	puts("2");
+		fscanf(fr, "%s", word.eng);
+		while(fscanf(fr, "%s", word.uzb) != EOF){
+//			if(!strcmp(lang,"eng"))
+				if(!strcmp(word.eng, sentence))
+					printf("| %-13s | %-13s |\n", word.eng, word.uzb);
+
+//			else if(!strcmp(lang,"uzb"))
+//				if(!strcmp(word.uzb, sentence))
+//					printf("| %-13s | %-13s |\n", word.eng, word.uzb);
+			fscanf(fr, "%s", word.eng);
+		}
+	}else if(lang == 1){
+	puts("1");
+		while(fscanf(fr, "%s", word.uzb) != EOF){
+			if(!strcmp(word.eng, sentence)){
+				fscanf(fr, "%s", word.eng);
+				printf("| %-13s | %-13s |\n", word.eng, word.uzb);
+			}
+		}
+	}
+
+	fclose(fr);
+}
+#endif
+void search_word(){
+	char sentence[32];
 	int i = 0;
 	WORD word;
 	int check;
+
 	puts("<<1>> English - Uzbek.");
 	puts("<<2>> Uzbek - English.");
 	printf(">>> "); scanf("%d", &check);
@@ -59,29 +103,25 @@ void search_word(){
 	FILE *fr = fopen("dictionary.txt", "r");
 
 	if(check == 1){
-		printf("Ingilizcha so'z kiriting>>> "); scanf("%s", eng);
-		while(eng[i] != '\0'){
-			eng[i] = tolower(eng[i]);
-			i++;
-		}
+		printf("Ingilizcha so'z kiriting>>> "); scanf("%s", sentence);
+		strcpy(sentence, to_lower(sentence));
+//		translate(sentence,"eng");
 
 		puts("+---------------+---------------+");
 		puts("| ENGLISH       | UZBEK         |");
 		puts("+---------------+---------------+");
 
 		while(fscanf(fr, "%s", word.eng) != EOF){
-			if(!(strcmp(word.eng, eng))){
+			if(!(strcmp(word.eng, sentence))){
 				fscanf(fr, "%s", word.uzb);
 				printf("| %-13s | %-13s |\n", word.eng, word.uzb);
 			}
 		}
 
 	}else if(check == 2){
-		printf("Uzbekcha so'z kiriting>>> "); scanf("%s", uzb);
-		while(uzb[i] != '\0'){
-			uzb[i] = tolower(uzb[i]);
-			i++;
-		}
+		printf("Uzbekcha so'z kiriting>>> "); scanf("%s", sentence);
+		strcpy(sentence, to_lower(sentence));
+//		translate(sentence,"uzb");
 
 		puts("+---------------+---------------+");
 		puts("| ENGLISH       | UZBEK         |");
@@ -89,7 +129,7 @@ void search_word(){
 
 		fscanf(fr, "%s", word.eng);
 		while(fscanf(fr, "%s", word.uzb) != EOF){
-			if(!(strcmp(word.uzb, uzb)))
+			if(!(strcmp(word.uzb, sentence)))
 				printf("| %-13s | %-13s |\n", word.eng, word.uzb);
 
 			fscanf(fr, "%s", word.eng);
@@ -108,14 +148,13 @@ int main(){
 		scanf("%d", &choice);
 
 		switch(choice){
-			case 1: add_new_word(); break;
-			case 2: search_word(); break;
-			case 3: show_words(); break;
-			case 4: exit(0); break;
+			case 1: /*system("clear");*/ add_new_word(); break;
+			case 2: /*system("clear");*/ search_word(); break;
+			case 3: /*system("clear");*/ show_words(); break;
+			case 0: /*system("clear");*/ exit(0); break;
 			default: printf("Iltimos menyudagilarnigina tanlang!\n"); break;
 		}
 	}
-
 
 	return 0;
 }
